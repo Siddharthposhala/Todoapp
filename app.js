9ac8931ccc72fa19c9f295149ec0be3baffe2288
Siddharthposhala <siddharthposhala020@gmail.com>
2022-12-18 16:48:32 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const express = require("express");
+const app = express();
+const { Todo } = require("./models");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.get("/", function (request, response) {
+  response.send("Hello World");
+});
+
+app.get("/todos", async function (_request, response) {
+  console.log("Processing list of all Todos ...");
+  try {
+    const todo = await Todo.findAll({ order: [["id", "ASC"]] });
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.get("/todos/:id", async function (request, response) {
+  try {
+    const todo = await Todo.findByPk(request.params.id);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.post("/todos", async function (request, response) {
+  try {
+    const todo = await Todo.addTodo(request.body);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async function (request, response) {
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.delete("/todos/:id", async function (request, response) {
+  try {
+    console.log("We have to delete a Todo with ID: ", request.params.id);
+
+    const todo = await Todo.destroy({
+      where: {
+        id: request.params.id,
+      },
+    });
+    response.send(todo ? true : false);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+module.exports = app;
